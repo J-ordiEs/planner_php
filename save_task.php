35c6c23025870f4758f271bc5bcf2c5f12ade232
J-ordiEs <jorge.tm.ink@outlook.com>
2020-10-26 20:51:16 +0100

    create planner

---
+++ save_task.php
@@ -0,0 +1,18 @@
+<?php 
+include ('db.php');
+if (isset($_POST['save_task'])){
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    
+    $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+    $resutl = mysqli_query($conn, $query);
+    if (!$resutl) {
+        die("Query failed");
+    }
+
+    echo 'saved';
+}
+
+
+
+?>
