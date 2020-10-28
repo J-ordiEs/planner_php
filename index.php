69469f684f44470f03ac90b04183811b2e5f63b8
J-ordiEs <jorge.tm.ink@outlook.com>
2020-10-28 19:24:05 +0100

    progress

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
                         <td><?php echo $row['description']?></td>
                         <td><?php echo $row['created_at']?></td>
                         <td>
-                            <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
+                            <a href="edit_task.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
                                 <i class="fas fa-pen"></i>
                             </a>
                             <a href="delete_task.php?id=<?php echo $row['id']?>" class="btn btn-danger">
